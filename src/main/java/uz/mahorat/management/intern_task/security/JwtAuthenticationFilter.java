4359791787571957a8213e61963b3fb7e0ca00c7
JavaScript0205 <javascript0222@gmail.com>
2025-08-11 13:17:17 +0500

    JwtAuthenticationFilter.java updated

--- src/main/java/uz/mahorat/management/intern_task/security/JwtAuthenticationFilter.java
+++ src/main/java/uz/mahorat/management/intern_task/security/JwtAuthenticationFilter.java
@@ -29,16 +29,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
-        final String jwt;
-        final String username;
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        jwt = authHeader.substring(7);
-        username = jwtUtil.extractUsername(jwt);
+        final String jwt = authHeader.substring(7);
+        final String username = jwtUtil.extractUsername(jwt);
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
